54d98c464c8f37da24090f427ad61f310f18c099
sofahmm <74815523+sofahmm@users.noreply.github.com>
2022-04-16 01:28:16 +0300

    Edit MenuUserPage Add ListHorsesPage

---
+++ KumysnayaFermaKursach/Pages/MenuUserPage.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace KumysnayaFermaKursach.Pages
+{
+    /// <summary>
+    /// Interaction logic for MenuUserPage.xaml
+    /// </summary>
+    public partial class MenuUserPage : Page
+    {
+        public MenuUserPage()
+        {
+            InitializeComponent();
+        }
+
+        private void HorsesList_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void OrdersBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ProductListBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void AboutUsBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void CreateOrderBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
